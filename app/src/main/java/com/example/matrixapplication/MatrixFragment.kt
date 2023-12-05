974970b1a38e03c9b2af61488493888743ca43ab
Valelaaa <kosneanuvaleriy@gmail.com>
2023-12-06 00:32:29 +0200

    - hardcoded matrix, little fixes

--- app/src/main/java/com/example/matrixapplication/MatrixFragment.kt
+++ app/src/main/java/com/example/matrixapplication/MatrixFragment.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 
 class MatrixFragment : Fragment() {
 
@@ -19,7 +22,33 @@ class MatrixFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_matrix, container, false)
+        val view = inflater.inflate(R.layout.fragment_matrix, container, false)
+
+        val loggingView: TextView = view.findViewById(R.id.logging_view)
+        var logingText = StringBuilder("")
+        val matrixTable: TableLayout = view.findViewById(R.id.matrix_table)
+        val rowsCount = 101
+        val columnsCount = 101
+        for (i in 0 until rowsCount) {
+            val tableRow = TableRow(requireContext())
+
+            for (j in 0 until columnsCount) {
+                val textView = TextView(requireContext())
+                textView.text = "[$i][$j]" // Здесь можно задать текст по умолчанию
+                logingText.append("[$i][$j]")
+                textView.setPadding(20, 10, 10, 10) // Установим отступы
+                textView.setBackgroundResource(R.drawable.border)
+                // Добавим TextView в TableRow
+                tableRow.addView(textView)
+            }
+
+            // Добавим TableRow в TableLayout
+            matrixTable.addView(tableRow)
+        }
+
+        loggingView.text = logingText
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
